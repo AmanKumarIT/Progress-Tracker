a10e05a2e5c528077ec7137fcbcea9c16572af1c
AmanKumarIT <akpailwar120@gmail.com>
2026-02-06 19:20:28 +0530

    DB connected

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -95,13 +99,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,  # required for most cloud Postgres
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
